d9581c7fba8ca72063fbbbdaf8cfcb11bdd84bd2
EphriamHab <ephremhabtamu2015@gmail.com>
2024-02-20 02:41:05 +0300

    index

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ app.use(express.json())
 
 const PORT = 3000
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
-app.get('/api/v1/tours')
 
-const getAllTous =  (req, res)=>{
+const getAllTours =  (req, res)=>{
   res.status(200).json({
     status:"Success",
     results:tours.length,
@@ -19,9 +18,8 @@ const getAllTous =  (req, res)=>{
     }
   })
 }
-app.get('/api/v1/tours', getAllTous)
 
-app.get('/api/v1/tours/:id', (req, res)=>{
+const getTour = (req, res)=>{
     console.log(req.params)
     const id = req.params.id * 1
     if(id > tours.length){
@@ -37,9 +35,9 @@ app.get('/api/v1/tours/:id', (req, res)=>{
           tour
       }
     })
-  })
+  }
 
-app.post('/api/v1/tours', (req, res)=>{
+const createTour= (req, res)=>{
 //    console.log(req.body)
 const newId = tours[tours.length-1].id + 1
 const newTour = Object.assign({id:newId}, req.body)
@@ -55,9 +53,9 @@ fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tour
    })
 })
 
-})
+}
 
-app.patch('/api/v1/tours/:id', (req,res)=>{
+const updateTour = (req,res)=>{
     const tourid = req.params.id * 1
     const tour = tours.find(tour=>tour.id = tourid)
 
@@ -75,7 +73,46 @@ app.patch('/api/v1/tours/:id', (req,res)=>{
            tour:updatedtour 
         }
     })
-})
+}
+const deleteTour = (req, res) => {
+    const tourId = req.params.id * 1;
+    const tourToDelete = tours.find(tour => tour.id === tourId);
+
+    if (!tourToDelete) {
+        return res.status(404).json({ 
+            status: "fail",
+            message: "Tour not found"
+         });
+    }
+
+    newTours = tours.filter(tour => tour.id !== tourId);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(newTours), err => {
+        if (err) return res.status(500).json({
+             status: "error", 
+             message: "Failed to delete tour"
+         });
+        res.status(204).json({
+             status: "success",
+             data:null
+         });
+    });
+}
+
+// app.get('/api/v1/tours', getAllTours)
+// app.get('/api/v1/tours/:id', getTour)
+// app.patch('/api/v1/tours/:id', updateTour)
+// app.post('/api/v1/tours', createTour)
+// app.get('/api/v1/tours/:id', deleteTour)
+
+app.route('/api/v1/tours')
+   .get(getAllTours)
+   .post(createTour)
+app.route('/api/v1/tours/:id')
+   .patch(updateTour)
+   .get(getTour)
+   .delete(deleteTour)
+
 
 
 
