548c196c85adea40324fbb37245de1771886d3f1
EphriamHab <ephremhabtamu2015@gmail.com>
2024-02-21 17:57:31 +0300

    Server

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const app = require('./app')
-const PORT = 3000
+/* eslint-disable prettier/prettier */
+const dotenv = require('dotenv')
 
+dotenv.config({path:'./config.env'})
+const app = require('./app')
+
+const PORT = process.env.PORT || 3000
+
+// eslint-disable-next-line prettier/prettier
 app.listen(PORT, ()=>{
+    // eslint-disable-next-line no-console
     console.log(`App started on ${PORT} `)
 })
