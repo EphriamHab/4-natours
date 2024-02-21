df61f71fe3094e276225a97e6240b395fc38360d
EphriamHab <ephremhabtamu2015@gmail.com>
2024-02-21 17:58:01 +0300

    Tour routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,10 +2,11 @@ const express = require('express')
 const tourController = require('../contollers/tourCotrollers')
 const router = express.Router()
 
+router.param('id',tourController.checkID )
 router
 .route('/')
 .get(tourController.getAllTours)
-.post(tourController.createTour)
+.post(tourController.checkBody, tourController.createTour)
 
 router
 .route('/:id')
