1f63993373888bf016982509442db968a7cc02e2
EphriamHab <ephremhabtamu2015@gmail.com>
2024-02-23 12:51:26 +0300

    Tour route modified

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,6 +3,7 @@ const tourController = require('../contollers/tourCotrollers')
 const router = express.Router()
 
 // router.param('id',tourController.checkID )
+router.route('/top-5-cheap').get(tourController.aliasTopTours,tourController.getAllTours)
 router
 .route('/')
 .get(tourController.getAllTours)
